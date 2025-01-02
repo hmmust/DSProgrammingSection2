c84fad7b9540650b4959f7182a21ecec74102aed
hmmust <146467005+hmmust@users.noreply.github.com>
2025-01-02 14:25:25 +0300

    multiprocessing2

--- part6/lect19e.py
+++ part6/lect19e.py
@@ -1,19 +1,22 @@
 import multiprocessing
 import time
-#print(multiprocessing.cpu_count())
-def printHi(n):
+print(multiprocessing.cpu_count())
+def printHi(n,queue):
+    sum =0
     for i in range(n):
-        time.sleep(0.5)
         print(f"hi{n}")
+        sum += i
+        time.sleep(0.5)
+    queue.put(sum)
 if __name__ == "__main__":
-    p1= multiprocessing.Process(target=printHi,args=(5,))
-    p2= multiprocessing.Process(target=printHi,args=(7,))
-    p3= multiprocessing.Process(target=printHi,args=(10,))
+    queue = multiprocessing.Queue()
+    p1 = multiprocessing.Process(target=printHi,args=(5,queue,))
+    p2 = multiprocessing.Process(target=printHi,args=(10,queue,))
     p1.start()
     p2.start()
-    p3.start()
     p1.join()
+    print(queue.get())
     p2.join()
-    p3.join()
-    print('done!')
-  
+    print(queue.get())
+
+    print("Done!")
